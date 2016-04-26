7b2431b97ca1b29ffa87cc9e399897196a179533
felixo <alexei.eremin@gmail.com>
2016-04-26 17:55:57 +0300

    3

--- fcgt/models.py
+++ fcgt/models.py
@@ -2,4 +2,20 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Gallery(models.Model):
+    art_host_name = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    art_city = models.CharField(max_length=200)
+    art_mail = models.CharField(max_length=200)
+    art_link = models.CharField(max_length=200)
+    art_category = models.CharField(max_length=200)
+    art_delete = models.BooleanField(default=False)
+    art_vote = models.IntegerField(default=0)
+
+
+
+class Vote(models.Model):
+    art = models.ForeignKey(Gallery, on_delete=models.CASCADE)
+    vote_id = models.CharField(max_length=200)
+    vote_is_it = models.BooleanField(default=False)
--- fcgt/views.py
+++ fcgt/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
+from .models import Gallery, Vote
+
+def index(request):
+    #question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'fcgt/index.html')
 
 
 # Create your views here.
