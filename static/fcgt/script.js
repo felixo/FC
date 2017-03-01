2be7e41d0f88617f9bb701ce120356b0c00fef08
alexey <alexei.eremin@gmail.com>
2017-03-01 15:43:40 +0100

    PreRelize

--- static/fcgt/script.js
+++ static/fcgt/script.js
@@ -1,6 +1,8 @@
  $(document).ready(function() {
 
-
+    //$('input[type="submit"]').prop('disabled', true);
+    $("input").prop('required',true);
+    $("select").prop('required',true);
     $( ".likeButtonText" ).click(function() {
      //alert( "The Function start" );
      var catid = 0;
@@ -17,5 +19,31 @@
 
            });
     });
+    /*$('input[type="submit"]').click(function(){
+        if ($('#div_art_host').var == ''){
+            alert ("Avada Kedavra");
+            //$('input[type="submit"]').prop('disabled', true);
+        }
+    });*/
+    /*$( "#more" ).click(function() {
+       alert('Avada Kedavra');
+       var url=$("#moreButton").attr("href");
+       alert(url);
+       $.get(url, {}, function(data){
+                alert("It's Gone");
+               $(".galleryWrap").append("<ul id='loadersub'></ul>");
+
+                $("#loadersub").load(url+" #loader", function(){
+                    var buffer = $("#loadersub").html();
+                    $(".loade").append(buffer);
+                    alert("Rocn'Roll");
+
+               });
+
+
+           });
+       return false;
+
+    });*/
 
 });
